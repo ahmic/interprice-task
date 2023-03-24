ee5128e2eaa591cbd80f723ffaa2c1654b122cb8
Amir Ahmic <am1r.root@gmail.com>
2023-03-24 21:07:49 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DashboardComponent />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DashboardComponent from './components/DashboardComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DashboardComponent
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
